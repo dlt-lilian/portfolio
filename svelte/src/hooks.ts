556a34358162bf096ca64a0ff3357b992b4d11b4
dlt-lilian <dulotlilian@gmail.com>
2025-08-17 15:05:33 +0200

    Ajout hooks

--- svelte/src/hooks.ts
+++ svelte/src/hooks.ts
@@ -1,3 +1,6 @@
 import { deLocalizeUrl } from '$lib/paraglide/runtime';
 
 export const reroute = (request) => deLocalizeUrl(request.url).pathname;
+
+// SvelteKit expects a transport object for client-side routing
+export const transport = {};
