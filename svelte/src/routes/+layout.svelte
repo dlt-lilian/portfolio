f52aa16de65055b578d0955629cf3e6db7715720
dlt-lilian <dulotlilian@gmai.com>
2026-01-01 18:50:34 +0100

    debug

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import '../app.css';
+    import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+    import Header from "$lib/components/navigation/Header.svelte";
+    import Footer from "$lib/components/navigation/Footer.svelte";
+
+    let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header />
+
+<main class="mt-10 mb-10 min-h-screen max-w-6xl mx-auto">
+    {@render children()}
+</main>
+
+<Footer />
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from "$lib/components/landing/Header.svelte";
+    import Projects from "$lib/components/landing/Projects.svelte";
+    import Skilltree from "$lib/components/landing/Skilltree.svelte";
+    import Learning from "$lib/components/landing/Learning.svelte";
+    import Experiences from "$lib/components/landing/Experiences.svelte";
+    import Contact from "$lib/components/landing/Contact.svelte";
+</script>
+
+<div class="space-y-4">
+
+    <Header />
+
+    <Projects />
+
+    <!--json a completer-->
+    <Skilltree/>
+
+    <!--Learning en cours-->
+    <Learning />
+
+    <!--json a completer-->
+    <Experiences />
+
+    <Contact />
+
+</div>
