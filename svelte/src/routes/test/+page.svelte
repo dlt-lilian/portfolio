560a2aad396e01053118a7e0dd09285bb65b65c8
Lilian <dulotlilian@gmail.com>
2026-01-13 16:53:41 +0100

    Test gallery

--- svelte/src/routes/test/+page.svelte
+++ svelte/src/routes/test/+page.svelte
@@ -1,1 +1,19 @@
-<p>Coucou</p>
+<script>
+</script>
+
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4 lg:px-100 md:px-16 px-5 my-10">
+    <div class="grid gap-4">
+        <img class="aspect-[4/3] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+        <img class="aspect-[3/4] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+    </div>
+
+    <div class="grid gap-4">
+        <img class="aspect-[3/4] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+        <img class="aspect-[4/3] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+    </div>
+
+    <div class="grid gap-4">
+        <img class="aspect-[4/3] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+        <img class="aspect-[3/4] w-full object-cover rounded-xl" src="https://picsum.photos/1920/1080">
+    </div>
+</div>
